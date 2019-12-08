36b4e918717d6209ee7e59c21737cf8fbdb17315
vnebo <volodymyr.neborachko@zealicsolutions.com>
2019-12-08 20:00:29 +0200

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            int[] i = new int[] { 1, 2, 3, 4, 5, 6, 757};
+            Program p = new Program();
+            int s = p.solution(i);
+            string s = "fdsafasdfd";
+            StringMap<TValue> 
+            Console.WriteLine(s);
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            return A.Where(s => s<9).Max();
+        }
+    }
+    public interface IStringMap<TValue> where TValue : class
+    {
+        /// <summary>Returns number of elements in a map</summary>
+        int Count { get; }
+
+        /// <summary>
+        /// If <c>GetValue</c> method is called but a given key is not in a map then <c>DefaultValue</c> is returned.
+        /// </summary>
+        TValue DefaultValue { get; set; }
+
+        /// <summary>
+        /// Adds a given key and value to a map.
+        /// If the given key already exists in the map, then the value associated with this key should be overridden.
+        /// </summary>
+        /// <returns>true if the value for the key was overridden, otherwise false</returns>
+        /// <exception cref="System.ArgumentNullException">If the key is null</exception>
+        /// <exception cref="System.ArgumentException">If the key is an empty string</exception>
+        /// <exception cref="System.ArgumentNullException">If the value is null</exception>
+        bool AddElement(string key, TValue value);
+
+        /// <summary>
+        /// Removes a given key and associated value from a map.
+        /// </summary>
+        /// <returns>true if the key was in the map and was removed, otherwise false</returns>
+        /// <exception cref="System.ArgumentNullException">If the key is null</exception>
+        /// <exception cref="System.ArgumentException">If the key is an empty string</exception>
+        bool RemoveElement(string key);
+
+        /// <summary>
+        /// Returns the value associated with a given key.
+        /// </summary>
+        /// <returns>The value associated with a given key or <c>DefaultValue</c> if the key does not exist in a map</returns>
+        /// <exception cref="System.ArgumentNullException">If the key is null</exception>
+        /// <exception cref="System.ArgumentException">If the key is an empty string</exception>
+        TValue GetValue(string key);
+    }
+    public class TValue
+    {
+        public object _tkey;
+        public string _tvalue;
+        public TValue(object tkey, string tvalue)
+        {
+            _tkey = tkey;
+            _tvalue = tvalue;
+        }
+        public object TKey { get; set; }
+        public string TValueString { get; set; }
+    }
+    public class StringMap<TValue> : IStringMap<TValue>
+        where TValue : class
+    {
+        private StringMap<TValue> _val;
+        public StringMap(StringMap<TValue> value){
+            _val = value;
+        }
+        /// <summary> Returns number of elements in a map</summary>
+        public int Count => _val.Count();
+
+        /// <summary>
+        /// If <c>GetValue</c> method is called but a given key is not in a map then <c>DefaultValue</c> is returned.
+        /// </summary>
+        public TValue DefaultValue
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds a given key and value to a map.
+        /// If the given key already exists in a map, then the value associated with this key should be overriden.
+        /// </summary>
+        /// <returns>true if the value for the key was overriden otherwise false</returns>
+        /// <exception cref="System.ArgumentNullException">If the key is null</exception>
+        /// <exception cref="System.ArgumentException">If the key is an empty string</exception>
+        /// <exception cref="System.ArgumentNullException">If the value is null</exception>
+        public bool AddElement(string key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Removes a given key and associated value from a map.
+        /// </summary>
+        /// <returns>true if the key was in the map and was removed otherwise false</returns>
+        /// <exception cref="System.ArgumentNullException">If the key is null</exception>
+        /// <exception cref="System.ArgumentException">If the key is an empty string</exception>
+        public bool RemoveElement(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns the value associated with a given key.
+        /// </summary>
+        /// <returns>The value associated with a given key or <c>DefaultValue</c> if the key does not exist in a map</returns>
+        /// <exception cref="System.ArgumentNullException">If a key is null</exception>
+        /// <exception cref="System.ArgumentException">If a key is an empty string</exception>
+        public TValue GetValue(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
